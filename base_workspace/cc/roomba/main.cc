3ac0d06f3ba55a3583b9ed6c465463e65c75b790
Diego Waxemberg <diego.waxemberg@gmail.com>
2015-12-05 17:49:20 -0800

    Added mario theme song :)

--- base_workspace/cc/roomba/main.cc
+++ base_workspace/cc/roomba/main.cc
@@ -2,6 +2,7 @@
 #include <memory>
 #include "cc/roomba/roomba_controller.h"
 #include "cc/roomba/roomba_controller_factory.h"
+#include "cc/roomba/config.h"
 
 #include <unistd.h>
 
@@ -9,6 +10,7 @@ using std::unique_ptr;
 using roomba::RoombaController;
 using roomba::BuildController;
 using roomba::Error;
+using roomba::kMarioThemeIntro;
 
 int main() {
   unique_ptr<RoombaController> controller;
@@ -23,19 +25,54 @@ int main() {
     return -1;
   }
   sleep(1);
+  error = controller->SetSong(0, kMarioThemeIntro);
+  if (!error) {
+    std::cout << error.error_message() << std::endl;
+    return -1;
+  }
+  sleep(1);
   error = controller->Safe();
   if (!error) {
     std::cout << error.error_message() << std::endl;
     return -1;
   }
   sleep(1);
+  error = controller->PlaySong(0);
+  if (!error) {
+    std::cout << error.error_message() << std::endl;
+    return -1;
+  }
   // Turn in place clockwise.
   error = controller->DriveDirect(250, -250);
   if (!error) {
     std::cout << error.error_message() << std::endl;
     return -1;
   }
-  sleep(1);
+  usleep(150 * 1000);
+  error = controller->DriveDirect(-250, 250);
+  if (!error) {
+    std::cout << error.error_message() << std::endl;
+    return -1;
+  }
+  usleep(150 * 1000);
+  error = controller->DriveDirect(250, 250);
+  if (!error) {
+    std::cout << error.error_message() << std::endl;
+    return -1;
+  }
+  usleep(450 * 1000);
+  error = controller->DriveDirect(-250, -250);
+  if (!error) {
+    std::cout << error.error_message() << std::endl;
+    return -1;
+  }
+  usleep(800 * 1000);
+  error = controller->DriveDirect(250, 250);
+  if (!error) {
+    std::cout << error.error_message() << std::endl;
+    return -1;
+  }
+  usleep(200 * 1000);
   error = controller->Start();
   if (!error) {
     std::cout << error.error_message() << std::endl;
